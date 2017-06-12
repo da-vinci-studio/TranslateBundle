963c1102c615ca2ff465d51243c6ff76100bdf14
Pawel Maniora <pmaniora@davinci-studio.eu>
2017-06-12 14:59:35 +0200

    di fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('dvs_core_authentication');
+        $treeBuilder->root('dvs_translate');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
